39ad0e4757da42fd40c0081b5874e06173894767
Varvara <v.kholmanskaya@yandex.ru>
2020-10-03 14:00:36 +0300

    feat:complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-
-exports.min = function min (array) {
+exports.min = function min(array) {
+  console.log(array);
+  if (!array || !array.length) {
   return 0;
-}
-
-exports.max = function max (array) {
+  } else {
+  return Math.min.apply(null, array);
+  }
+  }
+  
+  exports.max = function max(array) {
+  console.log(array);
+  if (!array || !array.length) {
   return 0;
-}
-
-exports.avg = function avg (array) {
+  } else {
+  return Math.max.apply(null, array);
+  }
+  }
+  
+  exports.avg = function avg(array) {
+  console.log(array);
+  if (!array || !array.length) {
   return 0;
-}
+  } else {
+  return array.reduce((current, next) => current + next, 0) / array.length;
+  }
+  }
